41800fc667889cecc519da2870a104f19988e8e7
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2023-08-24 04:02:05 +0100

    finished the css style for the preheader section

---
+++ frontend/src/components/PreHeader.jsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+
+const PreHeader = () => {
+  return (
+    <div className="pre-header">
+      <div className="container">
+        <div className="d-flex">
+          <ul className="pre-header-nav" role="menu">
+            <li>
+              <Link to="/">guides</Link>
+            </li>
+            <li>
+              <Link to="/">terms of sale</Link>
+            </li>
+            <li>
+              <Link to="/">terms of use</Link>
+            </li>
+            <li>
+              <Link to="/">privacy and policy</Link>
+            </li>
+          </ul>
+          <ul className="pre-header-nav" role="menu">
+            <li className="login">
+              <Link to="/">login</Link>
+            </li>
+            <li>
+              <Link to="/">register</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PreHeader;
--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
+import PreHeader from '../components/PreHeader';
 
 const RootLayout = () => {
   return (
     <>
+      <PreHeader />
       <Header />
       <Outlet />
     </>
