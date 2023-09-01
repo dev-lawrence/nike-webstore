449d9a167161ea09a95325b14d395725e042828f
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2023-09-01 11:02:23 +0100

    added new files and finished the popular section

--- frontend/data/data.js
+++ frontend/data/data.js
@@ -1,76 +1,65 @@
 const products = [
+  // Men shoes
   {
-    category: 'Men',
-    subcategories: [
-      {
-        name: 'Clothing',
-        products: [
-          {
-            title: "Men's Nike Dri-FIT T-Shirt",
-            description: 'Moisture-wicking athletic shirt for men',
-            price: 29.99,
-            images: ['image1.jpg', 'image2.jpg'],
-            isNew: false,
-            isFeatured: true,
-            thumbnailImages: ['thumbnail1.jpg'],
-            isJustIn: false,
-            isBestseller: true,
-          },
-          {
-            // Add more men's clothing products here
-          },
-        ],
-      },
-      {
-        name: 'Shoes',
-        products: [
-          {
-            title: "Men's Nike Air Max 270",
-            description: 'Stylish and comfortable sneakers for men',
-            price: 129.99,
-            images: ['image5.jpg', 'image6.jpg'],
-            isNew: true,
-            isFeatured: true,
-            thumbnailImages: ['thumbnail3.jpg'],
-            isJustIn: true,
-            isBestseller: false,
-          },
-          {
-            // Add more men's shoe products here
-          },
-        ],
-      },
-      {
-        name: 'Accessories',
-        products: [
-          {
-            title: "Men's Nike Classic Baseball Cap",
-            description: 'Stylish cap for men',
-            price: 19.99,
-            images: ['image9.jpg'],
-            isNew: false,
-            isFeatured: true,
-            thumbnailImages: ['thumbnail5.jpg'],
-            isJustIn: false,
-            isBestseller: true,
-          },
-          {
-            // Add more men's accessory products here
-          },
-        ],
-      },
-    ],
+    name: 'AJKO 1 Low',
+    subName: "Men's Shoes",
+    slug: 'ajko-1-low-mens-shoes-HNz9ps_abb1c5',
+    subcategory: 'shoe',
+    category: 'men',
+    isHighlyRated: false,
+    justIn: false,
+    price: 120,
+    description:
+      "Turn heads in the AJKO 1 Low. These kicks take MJ's original icon and repackage it in synthetic leather and canvas. With a decidedly relaxed look and connection to the '85 original, they'll earn nods of approval from longtime fans and brand new sneakerheads alike.",
+    image:
+      'https://res.cloudinary.com/dj9m3f8ux/image/upload/v1693374205/Nike%20webstore/Men/Shoes/ajiko/ajko-1-low-mens-shoes-HNz9ps_pmat8m.jpg',
   },
-  // Repeat the above structure for Women and Kids categories
+
   {
-    category: 'Women',
-    // ...
+    name: 'Air Jordan 5 Retro SE',
+    subName: "Men's Shoes",
+    slug: 'air-jordan-5-retro-se-mens-shoes-07Xgmw19_es40lt',
+    category: 'men',
+    subcategory: 'shoe',
+    isHighlyRated: true,
+    justIn: true,
+    price: 225,
+    description:
+      'With its mesh-inspired grid and distinct midsole design, the AJ5 is unmistakable. This version features a mix of full-grain and synthetic leather for texture, plus that iconic lace lock.',
+    image:
+      'https://res.cloudinary.com/dj9m3f8ux/image/upload/v1693376181/Nike%20webstore/Men/Shoes/air-jordan-5/air-jordan-5-retro-se-mens-shoes-07Xgmw_baijvf.jpg',
   },
+
   {
-    category: 'Kids',
-    // ...
+    name: 'Air Jordan 12 Retro',
+    subName: "Men's Shoes",
+    slug: 'air-jordan-12-retro-mens-shoes-9rPt0x_jfn2iy',
+    category: 'men',
+    subcategory: 'shoe',
+    isHighlyRated: true,
+    justIn: false,
+    price: 200,
+    description:
+      "No need to call for a ride—MJ's game-winning look from '96 will get you where you need to go. This AJ12 is the return of Tinker Hatfield's celebrated design, the first to bring Zoom Air cushioning to Jordan's signature line.",
+    image:
+      'https://res.cloudinary.com/dj9m3f8ux/image/upload/v1693379666/Nike%20webstore/Men/Shoes/air-jordan-12/air-jordan-12-retro-mens-shoes-9rPt0x_rxgmi6.jpg',
   },
+
+  // Men clothes
+
+  // Men accessories
+
+  // Women shoes
+
+  // Woman clothes
+
+  // Women accessories
+
+  // Kids shoes
+
+  // Kids clothes
+
+  // Kids accessories
 ];
 
-// Example usage:
-console.log(products);
+export default products;
