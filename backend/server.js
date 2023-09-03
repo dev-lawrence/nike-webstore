69678a3305c58ad5edfc3e5d45d13489bac843d7
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2023-09-03 18:29:39 +0100

    working on the product details page

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,15 @@ const app = express();
 // Enable CORS for all routes
 app.use(cors());
 
+// get all products
 app.get('/api/products', (req, res) => {
   res.send(data.products);
+});
+
+// get a product
+app.get('/api/products/slug/:slug', (req, res) => {
+  const product = data.products.find((p) => p.slug === req.params.slug);
+  res.send(product);
 });
 
 const port = process.env.PORT || 5000;
--- frontend/src/components/Popular.jsx
+++ frontend/src/components/Popular.jsx
@@ -3,20 +3,13 @@ import '@splidejs/splide/dist/css/splide.min.css';
 import Card from './Card.jsx';
 import { Loading } from './Loading.jsx';
 import useFetchData from '../hooks/useFetchData.jsx';
+
 const Popular = ({ title }) => {
   const {
     data: products,
     loading,
     error,
   } = useFetchData('http://localhost:5000/api/products');
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
 
   return (
     <section className="popular pt-section">
@@ -28,9 +21,9 @@ const Popular = ({ title }) => {
 
       <div className="products">
         {loading ? (
-          <Loading />
-        ) : error ? (
-          <p>Error: {error.message}</p>
+          <div>
+            <Loading />
+          </div>
         ) : (
           <Splide
             options={{
@@ -64,37 +57,6 @@ const Popular = ({ title }) => {
             })}
           </Splide>
         )}
-        {/* <Splide
-          options={{
-            perPage: 1,
-            pagination: false,
-            gap: '2rem',
-            drag: true,
-            mediaQuery: 'min',
-            speed: 800,
-            breakpoints: {
-              576: {
-                perPage: 2,
-              },
-
-              768: {
-                perPage: 3,
-              },
-
-              1200: {
-                perPage: 3,
-              },
-            },
-          }}
-        >
-          {products.map((product) => {
-            return (
-              <SplideSlide key={product.slug}>
-                <Card product={product} />
-              </SplideSlide>
-            );
-          })}
-        </Splide> */}
       </div>
     </section>
   );
