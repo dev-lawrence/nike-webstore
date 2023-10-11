867d3f087d4dacb3d19c4e957ef4069d9e2f84dd
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2023-10-11 15:28:32 +0100

    another commit

--- frontend/src/components/BreadCrumb.jsx
+++ frontend/src/components/BreadCrumb.jsx
@@ -42,7 +42,7 @@ const BreadCrumbs = ({ slug }) => {
       <span className="separator">{separator}</span>
     </div>
   );
-  // Add the additional breadcrumb to the beginning of the breadcrumbs array
+  // Add the additional breadcrumb to the beginning of the breadcrumb array
   crumbs.unshift(additionalCrumb);
   return <div>{crumbs}</div>;
 };
