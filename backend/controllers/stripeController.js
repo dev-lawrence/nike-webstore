a92a0fb3c40d7c14f11706d7580fb13802b9809a
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2023-10-20 01:23:13 +0100

    another commit

--- backend/controllers/stripeController.js
+++ backend/controllers/stripeController.js
@@ -138,6 +138,9 @@ export const getOrders = async (req, res) => {
 export const stripeWebHook = async (request, response) => {
   const sig = request.headers['stripe-signature'];
 
+  console.log(sig);
+  console.log(request.body);
+
   let event;
   let data;
   let eventType;
